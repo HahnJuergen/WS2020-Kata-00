47cbdc2f3c682585a42fe2a39124d702d465e92a
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-10 10:33:14 +0100

    Initial commit

---
+++ src/WhatsYourName.java
@@ -0,0 +1,11 @@
+public class WhatsYourName {
+
+    /**
+     * Lesen Sie in der main-Methode der Nutzer*innen in und begrüßen Sie
+     * diese anschließend durch eine personalisierte Willkommensnachricht.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
